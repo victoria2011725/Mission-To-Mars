a8d038c97b18a7c7cfccc6717bfcfe7500a6260c
victoria2011725 <vict.tyukova@gmail.com>
2026-02-07 13:32:46 +0000

    Moved variable declarations to the top

--- Story_2/Story_2.ino
+++ Story_2/Story_2.ino
@@ -1,15 +1,15 @@
 // C++ code
 //
+const int RED_LED = 12;
+const int GREEN_LED = 11;
 void setup()
 {
-  pinMode(12, OUTPUT);
-  pinMode(11,OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
 }
 
 void loop()
 {
-  const int RED_LED = 12;
-  const int GREEN_LED = 11;
   digitalWrite(RED_LED, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(RED_LED, LOW);

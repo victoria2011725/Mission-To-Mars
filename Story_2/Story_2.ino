431f49c3922ee14b33ced708f1bf90c35582fa99
victoria2011725 <vict.tyukova@gmail.com>
2026-02-16 12:51:21 +0000

    Made LED high to low transitions happen at the same time

--- Story_2/Story_2.ino
+++ Story_2/Story_2.ino
@@ -11,12 +11,10 @@ void setup()
 void loop()
 {
   digitalWrite(RED_LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
   
+  digitalWrite(RED_LED, LOW);
   digitalWrite(GREEN_LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(GREEN_LED, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }

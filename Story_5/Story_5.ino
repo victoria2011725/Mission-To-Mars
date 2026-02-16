f2915e45d13dff460ff380b829e28ee12c1b257e
victoria2011725 <vict.tyukova@gmail.com>
2026-02-16 15:20:36 +0000

    Swapped out while loops for cleaner if, else if , else statements

--- Story_5/Story_5.ino
+++ Story_5/Story_5.ino
@@ -2,10 +2,10 @@
 //
 const int Left_Rev = 12;
 const int Left_Fwd = 11;
-const int Left_Enable=10;
-const int Right_Enable=9;
-const int Right_Rev =8;
-const int Right_Fwd=7;
+const int Left_Enable= 10;
+const int Right_Enable = 9;
+const int Right_Rev = 8;
+const int Right_Fwd = 7;
 
 void forward()
 {
@@ -15,25 +15,25 @@ void forward()
   digitalWrite(Right_Fwd, HIGH);
   digitalWrite(Left_Rev, LOW);
   digitalWrite(Right_Rev, LOW);
-  }
+}
 
 void right()
 {
-  analogWrite(Left_Enable,200);
-  analogWrite(Right_Enable,120);
-  digitalWrite(Right_Fwd,HIGH);
-  digitalWrite(Left_Fwd,HIGH);
-  digitalWrite(Left_Rev,LOW);
-  digitalWrite(Right_Rev,LOW);
+  analogWrite(Left_Enable, 200);
+  analogWrite(Right_Enable, 120);
+  digitalWrite(Right_Fwd, HIGH);
+  digitalWrite(Left_Fwd, HIGH);
+  digitalWrite(Left_Rev, LOW);
+  digitalWrite(Right_Rev, LOW);
 }
 void left()
 {
-  analogWrite(Left_Enable,120);
-  analogWrite(Right_Enable,200);
-  digitalWrite(Right_Fwd,HIGH);
-  digitalWrite(Left_Fwd,HIGH);
-  digitalWrite(Left_Rev,LOW);
-  digitalWrite(Right_Rev,LOW);
+  analogWrite(Left_Enable, 120);
+  analogWrite(Right_Enable, 200);
+  digitalWrite(Right_Fwd, HIGH);
+  digitalWrite(Left_Fwd, HIGH);
+  digitalWrite(Left_Rev, LOW);
+  digitalWrite(Right_Rev, LOW);
   
 }
   
@@ -41,52 +41,44 @@ void left()
 void setup()
 {
   pinMode(Left_Rev, OUTPUT);
-  pinMode(Left_Fwd,OUTPUT);
-  pinMode(Left_Enable,OUTPUT);
-  pinMode(Right_Enable,OUTPUT);
-  pinMode(Right_Rev,OUTPUT);
-  pinMode(Right_Fwd,OUTPUT);
+  pinMode(Left_Fwd, OUTPUT);
+  pinMode(Left_Enable, OUTPUT);
+  pinMode(Right_Enable, OUTPUT);
+  pinMode(Right_Rev, OUTPUT);
+  pinMode(Right_Fwd, OUTPUT);
     
 }
 
-
+unsigned long recorded_time = millis();
 void loop() 
 {
   //2 seconds straight, 2 seconds left, 2 seconds right, 2 left, 2 right
-  
-  unsigned long recorded_time = millis();
-  while (millis()-recorded_time<2000)
+  if (millis()-recorded_time < 2000)
   {
-    forward();    
+    forward();
   }
   
-  recorded_time = millis();
-  while (millis()-recorded_time<2000)
+  else if (millis()-recorded_time < 4000)
   {
-    left();    
+    left();
   }
-
-  recorded_time = millis();
-  while (millis()-recorded_time<2000)
+  else if (millis()-recorded_time < 6000)
   {
-    right();    
+    right();
   }
-  
-  
-  recorded_time = millis();
-  while (millis()-recorded_time<2000)
+  else if (millis()-recorded_time < 8000)
   {
-    left();    
+    left();
   }
-
-  recorded_time = millis();
-  while (millis()-recorded_time<2000)
+  else if (millis()-recorded_time < 10000)
   {
-    right();    
+    right();
   }
-    
-  
-   
-  
+  // reset when 10 seconds have past 
+  else
+  {
+    recorded_time = millis();
+  }
 }
-  
+
+ 

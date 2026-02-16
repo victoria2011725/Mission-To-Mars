2e7eeabbb9a1ce042380319a5135e0798c969e45
victoria2011725 <vict.tyukova@gmail.com>
2026-02-16 13:32:22 +0000

    Replaced millis code with delays and made indentation consistent

--- Story_7/Story_7.ino
+++ Story_7/Story_7.ino
@@ -1,10 +1,9 @@
 const int Left_Rev = 12;
 const int Left_Fwd = 11;
-const int Left_Enable=10;
-const int Right_Enable=9;
-const int Right_Rev =8;
-const int Right_Fwd=7;
-
+const int Left_Enable = 10;
+const int Right_Enable = 9;
+const int Right_Rev = 8;
+const int Right_Fwd = 7;
 
 void forward()
 {
@@ -14,7 +13,7 @@ void forward()
   digitalWrite(Right_Fwd, HIGH);
   digitalWrite(Left_Rev, LOW);
   digitalWrite(Right_Rev, LOW);
-  }
+}
 void stop()
 { 
   digitalWrite(Left_Enable, LOW);
@@ -24,7 +23,8 @@ void stop()
   digitalWrite(Left_Rev, LOW);
   digitalWrite(Right_Rev, LOW);
 }
-void left() {
+void left() 
+{
   digitalWrite(Left_Enable, HIGH);
   digitalWrite(Right_Enable, HIGH);
   digitalWrite(Left_Fwd, LOW);
@@ -41,47 +41,39 @@ void right()
   digitalWrite(Left_Rev, LOW);
   digitalWrite(Right_Rev, HIGH);
 }
-void figure_of_eight(){
-  unsigned long time;
-  for (int i = 0; i < 4; i++) {
-    time = millis();
-    while (millis() - time < 2000)
-    { forward(); }
-    stop(); 
-
-    time = millis();
-    while (millis() - time < 850)
-    { left(); }
-    stop();
+void figure_of_eight()
+{
+  for (int i = 0; i < 4; i++) 
+  {
+    forward();
+    delay(2000);
+    left();
+    delay(850);
   }
-  for (int i = 0; i < 4; i++) {
-    time = millis();
-    while (millis() - time < 850)
-    { right();}
-    stop();
-    
-    time = millis();
-    while (millis() - time < 2000)
-    { forward(); }
-    stop();
+  for (int i = 0; i < 4; i++) 
+  {
+    right();
+    delay(850);
+    forward();
+    delay(2000);
   }
-  }
+}
 
 
 void setup()
 {
   pinMode(Left_Rev, OUTPUT);
-  pinMode(Left_Fwd,OUTPUT);
-  pinMode(Left_Enable,OUTPUT);
-  pinMode(Right_Enable,OUTPUT);
-  pinMode(Right_Rev,OUTPUT);
-  pinMode(Right_Fwd,OUTPUT);
+  pinMode(Left_Fwd, OUTPUT);
+  pinMode(Left_Enable, OUTPUT);
+  pinMode(Right_Enable, OUTPUT);
+  pinMode(Right_Rev, OUTPUT);
+  pinMode(Right_Fwd, OUTPUT);
   
   figure_of_eight();
 }
 
-void loop(){
- 
+void loop()
+{
 }
 
 
